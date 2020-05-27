7145c7f9a57a900915aeca874ac1fccc051e44c4
MateusFernandes25 <64925284+MateusFernandes25@users.noreply.github.com>
2020-05-27 16:39:55 -0300

    Add files via upload

---
+++ software-rasp/gtk/main.py
@@ -0,0 +1,19 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class Handler:
+    def OnDestroy(self,*args):
+        Gtk.main_quit()
+        
+
+builder = Gtk.Builder()
+builder.add_from_file("main.glade")
+builder.connect_signals(Handler())
+
+window = builder.get_object("window")
+window.show_all()
+
+Gtk.main()
