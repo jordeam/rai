b18ac72b46f47799a9aa291e36f3abc84385f870
Eric Kurachi <erictkurachi@gmail.com>
2020-05-27 18:34:01 -0300

    Added first interactive prototype

--- software-rasp/web/README.md
+++ software-rasp/web/README.md
@@ -3,6 +3,7 @@
 No terminal do linux digite:
 * $ export FLASK_APP=flaskr
 * $ export FLASK_ENV=development
+* $ flask init-db (para iniciar um banco de dados vazio)
 * $ flask run
 
 Basta entrar no navegador com o endereço http://127.0.0.1:5000/hello
--- software-rasp/web/flaskr/__init__.py
+++ software-rasp/web/flaskr/__init__.py
@@ -28,23 +28,16 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return '''
-            <!doctype html>
-        <title>RAI</title>
-        <link rel="stylesheet" href="{{ url_for('static', filename='style.css') }}">
-        <nav>
-          <h1>Respirador Artificial Integrado</h1>
-          <ul>
-            <li><a href="{{ url_for('auth.login') }}">Log In</a>
-            <li><a href="{{ url_for('auth.register') }}">Register</a>
-          </ul>
-        </nav>
-        <section class="content">
-          <header>
-            Descrição
-          </header>
-            Dados
-        </section>
-        '''
+        return 'Hello World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
