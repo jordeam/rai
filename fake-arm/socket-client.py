cd00d89719ba16d04ca05cc06ad0e4f1fb8c56f9
JRM <jordeam@gmail.com>
2020-06-29 18:57:08 -0300

    Solving socket closing on client side. Adequating socket-client.py to python3 syntax.

--- fake-arm/socket-client.py
+++ fake-arm/socket-client.py
@@ -5,35 +5,35 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 5000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+server_address = ('localhost', 5002)
+print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 try:
     
     # Send data
-    message = 'r:mode'
-    print >>sys.stderr, 'sending "%s"' % message
+    message = b'r:mode'
+    print('sending "%s"' % message)
     sock.sendall(message)
 
     data = sock.recv(1024)
-    print >>sys.stderr, 'received "%s"' % data
+    print('received "%s"' % data)
 
     # Send data
-    message = 'r:mode! 1'
-    print >>sys.stderr, 'sending "%s"' % message
+    message = b'r:mode! 1'
+    print('sending "%s"' % message)
     sock.sendall(message)
 
     data = sock.recv(1024)
-    print >>sys.stderr, 'received "%s"' % data
+    print('received "%s"' % data)
 
     # Send data
-    message = 'r:mode'
-    print >>sys.stderr, 'sending "%s"' % message
+    message = b'r:mode'
+    print('sending "%s"' % message)
     sock.sendall(message)
 
     data = sock.recv(1024)
-    print >>sys.stderr, 'received "%s"' % data
+    print('received "%s"' % data)
 
 finally:
-    print >>sys.stderr, 'closing socket'
+    print('closing socket')
     sock.close()
